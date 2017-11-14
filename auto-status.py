60005e8dfa67556f5b7b23a981b64136b8b24816
christopherBar <cbar@stargate.cat.pdx.edu>
2017-11-14 02:12:00 -0800

    added email support

--- auto-status.py
+++ auto-status.py
@@ -5,6 +5,7 @@
 import csv
 import psycopg2
 import pprint
+import smtplib
 
 # ----------------------------------------------------------------------------- 
 # the location of the db credentials.
@@ -121,32 +122,90 @@ def queryDatabase(credentialFile, query, filename, outputVar):
     conn.close()
 
 # ----------------------------------------------------------------------------- 
-# Get Login Data from Swosqz
+# Email Departments about User Progress
 
+def sendEmail(dictionary, key):
+    sender = 'cbar@cat.pdx.edu'
+    receivers = ['cbar@cat.pdx.edu']
+
+    message = """From: Christopher Bartlett <cbar@cat.pdx.edu>
+To: $person <cbar@cat.pdx.edu>
+Subject: Automated SWOS Status Report for """
+
+    message += str((dictionary[key])[0])
+
+    header = ["\nQuizName, UserID, LastName, FirstName, Username, Email, Score, PassingScore\
+            Passed (recieved a passing score)",\
+            "Failed (has yet to pass the quiz)",\
+            "Awol (has not tried the quiz yet)",\
+            "No Login Data (has not logged into SWOS)"]
+
+    # calculate the length of the entry
+    length = 0
+    for entry in dictionary[key]:
+        length += len(entry)
+
+    # if the length is 1, than its just the 
+    # source (dont send an email no users remain)
+    if (length <= 1):
+        return
+
+    # print out all user attempts and headers
+    for entry,ind in zip((dictionary[key])[1:], range(0,4)):
+       
+        if (len(entry) > 0):
+            message += header[ind] + '\n'
+        
+        # go through all the attempts and decide
+        # whether they can be output into the email
+        for j in entry:
+
+            unique = True
+            
+            with open("passedUsers.csv", 'a+') as inp:
+                reader = csv.reader(inp, delimiter=',')
+                for line in reader: 
+                    if (j == line[5]):
+                        unique = False 
+
+                if (unique):
+                    message += '\t' + str(j) + '\n'
+
+            # if this is the people that passed the quiz save 
+            # all of their entries so they are never spammed again
+            if (ind == 0 and unique == True): 
+                with open("passedUsers.csv", 'a+') as outp:
+                    writer = csv.writer(outp, delimiter=',')
+                    writer.writerow(j)
+
+    # send the email, if it fails catch the exception
+    try:
+        smtpObj = smtplib.SMTP('localhost')
+        smtpObj.sendmail(sender, receivers, message)         
+        print "Successfully sent email"
+    except SMTPException:
+        print "Error: unable to send email"
+
+# ----------------------------------------------------------------------------- 
+# Query all the Databases
+
+# Get Login Data from Swosqz
 loginData = [LastLogin,]
 queryDatabase(swosqz, getLastLogin, "lastLogin", loginData)
 
-# ----------------------------------------------------------------------------- 
 # Get Score Data from Swosqz
-
 attemptData = [AllAttempts,]
 queryDatabase(swosqz, getAllAttempts, "attemptInfo", attemptData)
 
-# ----------------------------------------------------------------------------- 
 # Get QuizId, Name from Swosqz
-
 quizData = [QuizInfo,]
 queryDatabase(swosqz, getQuizInfo, "quizInfo", quizData)
 
-# ----------------------------------------------------------------------------- 
 # Get passing score from Swosqz
-
 moduleData = [PassingScore,]
 queryDatabase(swosqz, getPassingScore, "moduleInfo", moduleData)
 
-# ----------------------------------------------------------------------------- 
 # Get dept+source from Intranet
-
 sourceData = [Source,]
 queryDatabase(intranet, getSource, "sourceInfo", sourceData)
 
@@ -161,19 +220,11 @@ for quiz in quizData[1:]:
     quizData     = []
     passingScore = 0.0
     
-    # print (quiz[2])
-
-    # print(passingData)
-
-    # find the right entry, check that the item is a quiz,
-    # and has the same name as the current quiz (skip header)
+    # find the right entry/score, check that the item is a quiz,
+    # and has the same name as the current quiz (skip headers)
     for entry in moduleData[1:]:
-        
-        # if the item is the same name as the quiz
-        # has the same id as the quiz, and is a quiz...
         if (entry[MODULETYPE] == "quiz" and \
                 entry[MODULENAME] == quiz[QUIZNAME]):
-            
             passingScore = entry[PASSINGSCORE]
             break
 
@@ -189,50 +240,37 @@ for quiz in quizData[1:]:
         # particular user has had an attempt of not
         found = False
 
-        # print(user)
-
         # set the data variable to contain all user info
         data = [user[USERUID], user[USERLAST], user[USERFIRST], user[USERUNAME], user[USERMAIL]]
         
-        # for all attempts (skipping the headerline)...
+        # for all attempts (skipping the headerline) 
+        # check to see that they are on the current quiz,
+        # then check their score with the passingScore
         for attempt in attemptData[1:]:
-   
-            # print("attempt = " + str(attempt))
-
-            # check to see if the quiz 
             if (attempt[ATTEMPTUID] == user[USERUID]):
-
-                # if the attempt is the same as the quiz that was taken
-                # append it to that quizzes list
                 if (attempt[ATTEMPTQID] == quiz[QUIZQID]):
-                    
-                    # if they have recieved a passing grade        
+                   
+                    # if their score is higher, they go into the first list
                     if (attempt[ATTEMPTSCORE] >= passingScore):
-                        
-                        # enter teh user into the first category
                         quizData[DATAPASS] += [data + [int(attempt[ATTEMPTSCORE])] + [int(passingScore)],]
                         found = True
-
-                    # else add it to the made an attempt
+                    # else they go into the second list (not yet passed)
                     else:
-                        # enter the user into the second category 
-                        # (they havent met the goal)
                         quizData[DATAFAIL] += [data + [int(attempt[ATTEMPTSCORE])] + [int(passingScore)],]
                         found = True
 
-        # if the found variable is false, we didnt find it 
-        # attempts, if their last login time was 0, 
-        # they havent gotten on the quizsite
+        # if found is false, we need to add them to the other lists
         if (found == False):
 
             # if the users login time is greater than zero
-            # add them to the users that are AWOL.
+            # add them to the users that are AWOL (no quiz attempts).
             if (user[USERLOG] > 0):
                 quizData[DATAAWOL] += [data]
+            # else they havent gotten on the quizsite
             else:
                 quizData[DATANLOG] += [data]
 
-    # add the quiz data to the entire list of data
+    # add the quiz data to the list for all attempts
     condenseData += [quizData]
 
 # ----------------------------------------------------------------------------- 
@@ -250,6 +288,7 @@ with open(tempFile, 'w+') as out:
 # ----------------------------------------------------------------------------- 
 # Print out the results of the queries 
 
+"""
 print(condenseData)
 
 for quiz in condenseData:
@@ -262,6 +301,7 @@ for quiz in condenseData:
     print("\tNoLogin:\n\t"          + str(quiz[4]))
 
 print("\n\n")
+"""
 
 # ----------------------------------------------------------------------------- 
 # Put the users into dictionaries
@@ -285,35 +325,31 @@ for source in sourceUsers:
 
 # ----------------------------------------------------------------------------- 
 
-"""for all users in the source list, look for all instances of them in the condensed data set, 
-append all of those attempts (p/f/a) into the output data directory under the user namd that 
-they where added by (use email as the key that ties them together)"""
-
-#Source = ["last_name", "first_name", "email", "dept", "source"]
-#data = [user[USERUID], user[USERLAST], user[USERFIRST], user[USERUNAME], user[USERMAIL]]
-for user in sourceData[1:]:
-    
+# for all the people that added people, add people they added 
+# to a dictionary in the same format as the condensed data
+for user in sourceData[1:]:    
     for quiz in condenseData:
+
+        # for the passed, failed, awol, and nolog lists in quiz...
         for i in range(1,4):
             for attempt in quiz[i]:
-
-                # print(user, attempt)
-
+                # if the user was added by someone on the list...
                 if (user[2] == attempt[4]):
 
-                    # put that data into the the 
-                    # output data in the right 
+                    # put that data into the the output data in the right list
                     (OutputData[user[4]])[i] += [[quiz[0]] + attempt]
 
+# ----------------------------------------------------------------------------- 
+# Print the Dictionary
 
-# print(sourceData)
-
-for key,entry in OutputData.items():
-    pprint.pprint(str(key) + "\n" + str(entry))
+# print all of the entries in the dictionary (with keys)
+# for key,entry in OutputData.items():
+    # pprint.pprint(str(key) + "\n" + str(entry))
 
 # ----------------------------------------------------------------------------- 
-# Email Departments about User Progress
-# ----------------------------------------------------------------------------- 
+# Email all User Sources
+
+for i in OutputData:
+    sendEmail(OutputData, i)
 
 
-# ----------------------------------------------------------------------------- 
